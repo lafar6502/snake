1b516efa0bf5aea4408e1a4d533f601539aae749
lafar6502 <rasputnik6502@gmail.com>
2023-03-26 20:13:53 +0200

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,30 @@
 function kopiujEkranDoNeopixel () {
     for (let x = 0; x <= wys - 1; x++) {
-        for (let x2 = 0; x2 <= szer - 1; x2++) {
-            let y = 0
+        for (let y = 0; y <= szer - 1; y++) {
             if (y % 2 == 1) {
-                strip.setMatrixColor(0, 0, neopixel.colors(NeoPixelColors.Red))
+                strip.setMatrixColor(szer - 1 - x, y, getPix(x, y))
             } else {
-                strip.setMatrixColor(x2, y, neopixel.colors(NeoPixelColors.Red))
+                strip.setMatrixColor(x, y, getPix(x, y))
             }
         }
     }
+    strip.show()
 }
 function setPix (x: number, y: number, color: number) {
     ekran[y * szer + x] = color
 }
+function wyczyscEkran () {
+	
+}
+input.onButtonPressed(Button.A, function () {
+    for (let x = 0; x <= szer - 1; x++) {
+        setPix(x, x, neopixel.rgb(100, 255, x * 10))
+        setPix(szer - 1 - x, x, neopixel.rgb(255, x * 10, x * 10))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
 function getPix (x: number, y: number) {
     return ekran[y * szer + x]
 }
@@ -31,5 +43,6 @@ for (let x3 = 0; x3 <= szer * wys; x3++) {
     ekran[x3] = 0
 }
 basic.forever(function () {
-	
+    basic.pause(200)
+    kopiujEkranDoNeopixel()
 })
