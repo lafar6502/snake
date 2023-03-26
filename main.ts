946cfc51ef93213ad3b70adc6f703fc018cc7d5b
lafar6502 <rasputnik6502@gmail.com>
2023-03-26 21:42:31 +0200

    Aktualizacja pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,9 @@ function setPix (x: number, y: number, color: number) {
     ekran[y * szer + x] = color
 }
 function idzieWaz () {
+    if (awaria) {
+        return
+    }
     if (dir == 1) {
         snakeX += 1
     } else if (dir == 2) {
@@ -25,6 +28,14 @@ function idzieWaz () {
     }
     snakeX = (snakeX % szer + szer) % szer
     snakeY = (snakeY % wys + wys) % wys
+    k = kolizja()
+    if (k == 1) {
+        dlugosc += 1
+        losujZarcie()
+    } else if (k == 2) {
+        awaria = 1
+        return
+    }
     waz.push(snakeX)
     waz.push(snakeY)
     while (waz.length > 2 * dlugosc) {
@@ -33,13 +44,24 @@ function idzieWaz () {
     }
 }
 function wyczyscEkran () {
-    for (let x3 = 0; x3 <= szer * wys; x3++) {
-        ekran[x3] = 0
+    for (let x2 = 0; x2 <= szer * wys; x2++) {
+        ekran[x2] = 0
     }
 }
 input.onButtonPressed(Button.A, function () {
     dir = ((dir - 1) % 4 + 4) % 4
 })
+function kolizja () {
+    p = getPix(snakeX, snakeY)
+    if (p == 0) {
+        return 0
+    }
+    serial.writeValue("kolizja", snakeX)
+    if (snakeX == zarcieX && snakeY == zarcieY) {
+        return 1
+    }
+    return 2
+}
 function debug () {
     serial.writeValue("x", snakeX)
     serial.writeValue("y", snakeY)
@@ -51,14 +73,32 @@ input.onButtonPressed(Button.B, function () {
 function getPix (x: number, y: number) {
     return ekran[y * szer + x]
 }
+function losujZarcie () {
+    zarcieY = randint(0, wys - 1)
+    zarcieX = randint(0, szer - 1)
+    while (getPix(zarcieX, zarcieY) != 0) {
+        zarcieY = randint(0, wys - 1)
+        zarcieX = randint(0, szer - 1)
+    }
+}
 function rysujWaz () {
     indeks = 0
+    c = neopixel.rgb(255, 3, 144)
+    if (awaria) {
+        c = neopixel.rgb(80, 80, 80)
+    }
     while (indeks < waz.length) {
-        setPix(waz[indeks], waz[indeks + 1], neopixel.rgb(255, 3, 144))
+        setPix(waz[indeks], waz[indeks + 1], c)
         indeks += 2
     }
 }
+let c = 0
 let indeks = 0
+let zarcieY = 0
+let zarcieX = 0
+let p = 0
+let k = 0
+let awaria = 0
 let snakeX = 0
 let waz: number[] = []
 let dir = 0
@@ -81,13 +121,14 @@ snakeY = 10
 dlugosc = 3
 dir = 1
 waz = [snakeX, snakeY]
+losujZarcie()
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
     idzieWaz()
     wyczyscEkran()
     rysujWaz()
+    setPix(zarcieX, zarcieY, neopixel.rgb(0, 255, 0))
     kopiujEkranDoNeopixel()
-    debug()
-    basic.pause(100)
+    basic.pause(50)
 })
